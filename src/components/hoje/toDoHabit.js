85ecc7bdc3e323c93c6f43aa8ffd2e65d9460249
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-05-28 08:38:15 -0300

    marca e desmarca habito como feito implementados  Changes to be committed: 	modified:   src/components/hoje/toDoHabit.js

--- src/components/hoje/toDoHabit.js
+++ src/components/hoje/toDoHabit.js
@@ -1,5 +1,40 @@
 import styled from "styled-components";
+import { useContext,useState } from "react";
+import UserContext from "../../contexts";
+import axios from "axios";
+
 export default function ToDoHabit(props){
+    const { header } =useContext(UserContext);
+    const [isLoading,setLoading]=useState(false);
+    function toggleDone(){
+        if(!isLoading){
+            if(props.done){
+                setLoading(true);
+                const requisition=axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${props.id}/uncheck`,{}, header.config)
+                requisition.then(response=>{
+                    
+                    setLoading(false);
+                    props.requestTodayHabits()
+                });
+                requisition.catch(error=>{
+                    alert("algo deu ruim")
+                    console.log(error.data)
+                });
+            }
+            if(!props.done){
+                setLoading(true);
+                const requisition=axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${props.id}/check`,{}, header.config)
+                requisition.then(response=>{
+                    setLoading(false);
+                    props.requestTodayHabits()
+                });
+                requisition.catch(error=>{
+                    alert("algo deu ruim")
+                    console.log(error.data)
+                });
+            }
+        }
+    }
     return(
         <Container>
             <div>
@@ -8,7 +43,7 @@ export default function ToDoHabit(props){
                 <p>Sequencia atual: <span>{props.currentSequence} dias</span></p>
                 <p>Seu recorde: <span>{props.highestSequence} dias</span></p>
             </div>
-            <IconWrapper done={props.done}>
+            <IconWrapper done={props.done} onClick={toggleDone}>
              <ion-icon name="checkbox"></ion-icon>
             </IconWrapper>
         </Container>
